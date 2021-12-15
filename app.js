356600b08819807df99b3140279952230fc651e9
markuswatermeyer <42544459+markuswatermeyer@users.noreply.github.com>
2021-12-15 19:15:51 +0100

    Another Change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Markus!'));
+app.get('/', (req, res) => res.send('HalliHallo, Markus!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
